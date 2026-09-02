2b41bffa287ef67495aeb60eb19494c08564587c
JagerDarkOuice <ashersutton2007@gmail.com>
2026-09-02 17:36:43 -0400

    Had to change to pyautogui because my os doesnt support the mouse lib.

--- src/joymouse.py
+++ src/joymouse.py
@@ -1,18 +1,18 @@
 import serial
-import mouse, sys
+import pyautogui
 import time 
 
-mouse.FAILSAFE=False
+pyautogui.FAILSAFE=False
 ArduinoSerial = serial.Serial('com6',115200)
 time.sleep(1)
 button_was_pressed = False
 while 1:
     data=str(ArduinoSerial.readline().decode('ascii'))
     (x,y,z)=data.split(":")
-    (X,Y)=mouse.get_position()  
+    (X,Y)=pyautogui.position()  
     (x,y)=(int(x),int(y))
-    mouse.move(X+x,Y+y)
+    pyautogui.moveTo(X+x,Y+y)
     button_is_pressed = z.strip() == "1"
     if button_is_pressed and not button_was_pressed:
-        mouse.click(button="middle")
+        pyautogui.click(button="middle")
     button_was_pressed = button_is_pressed
